1cd7f940cca8b1c1c65ffe29bfa09432c823f0ae
Eka Ariyansyah <ekaariyansyah@ymail.com>
2020-06-25 10:13:00 +0700

    move path to method

--- service/bukopinmobile-service-core/src/main/java/id/co/asyst/bukopin/mobile/service/core/PushNotificationService.java
+++ service/bukopinmobile-service-core/src/main/java/id/co/asyst/bukopin/mobile/service/core/PushNotificationService.java
@@ -24,9 +24,9 @@ import retrofit2.http.POST;
  * @version $Revision$, Jun 2, 2020
  * @since 1.2.Alpha1
  */
-@ServiceUrl("https://fcm.googleapis.com/fcm/send")
+@ServiceUrl("https://fcm.googleapis.com")
 public interface PushNotificationService {
 
-    @POST("/")
+    @POST("/fcm/send")
     Call<Object> pushNotif(@Header("Authorization") String auth, @Header("Content-Type") String contentType, @Body Object req);
 }
